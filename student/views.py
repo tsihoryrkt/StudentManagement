a51ae02c77ebab34a51e9fca613b83bd5723854d
tsihoryrkt <tsihoryrkt@gmail.com>
2024-04-15 22:12:54 +0300

    Student form: adding new user and save to dbsqlite

--- student/views.py
+++ student/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .forms import StudentRegistration
+from .models import User
 
 # Create your views here.
+
+def student_form(request):
+    if request.method == 'POST':
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm, email=em, password=pw)
+            reg.save()
+            fm = StudentRegistration()
+    else:
+        fm = StudentRegistration()
+    return render(request,'student/studentform.html', {'form':fm})
